7625d616d6d8170b252ae7f2a5af2bb30002aa6e
Abdul Ibrahim <theoneadisa@gmail.com>
2022-07-31 23:51:39 +0100

    Update computer-vision.ts

--- Self Driving Car/computer-vision.ts
+++ Self Driving Car/computer-vision.ts
@@ -1,7 +1,10 @@
 export function getObstacleEvents() {
+  
   const coinFlip = Boolean(Math.random() > 0.5);
+  
   return { 
     'ObstacleLeft': coinFlip, 
     'ObstacleRight': !coinFlip 
   };
+  
 }
